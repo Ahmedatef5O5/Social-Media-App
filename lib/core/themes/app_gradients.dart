cd4fe013c4aed27bdd8d411bdc0dd560be32310d
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-05 00:57:03 +0200

    feat: implement dual-corner background gradient using triple stops

---
+++ lib/core/themes/app_gradients.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_app/core/themes/app_colors.dart';
+
+class AppGradients {
+  static const LinearGradient doubleCircleGradient = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [AppColors.bgColor, AppColors.white, AppColors.bgColor],
+    stops: [0.0, 0.5, 1.0],
+  );
+}
--- lib/core/themes/app_themes.dart
+++ lib/core/themes/app_themes.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/core/themes/app_colors.dart';
 
 class AppThemes {
   static ThemeData lightTheme = ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    useMaterial3: true,
+    scaffoldBackgroundColor: AppColors.bgColor,
+    primaryColor: AppColors.primaryColor,
+    appBarTheme: AppBarTheme(backgroundColor: AppColors.bgColor, elevation: 0),
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primaryColor,
+      surface: AppColors.bgColor,
+    ),
   );
 }
