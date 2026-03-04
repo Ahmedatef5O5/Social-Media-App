96cace3ed53b0e969c3fe54b3c206ab8327b816a
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-04 22:53:57 +0200

    feat: add UserData model with fromMap and toMap methods

---
+++ lib/features/auth/data/models/user_data.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+class UserData {
+  final String id;
+  final String name;
+  final String username;
+  final String email;
+  final String? title;
+  final String? imageUrl;
+
+  const UserData({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    this.title,
+    this.imageUrl,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'username': username,
+      'email': email,
+      'title': title,
+      'imageUrl': imageUrl,
+    };
+  }
+
+  factory UserData.fromMap(Map<String, dynamic> map) {
+    return UserData(
+      id: map['id'] as String? ?? '',
+      name: map['name'] as String? ?? '',
+      username: map['username'] as String? ?? '',
+      email: map['email'] as String,
+      title: map['title'] != null ? map['title'] as String? ?? '' : null,
+      imageUrl:
+          map['imageUrl'] != null ? map['imageUrl'] as String? ?? '' : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserData.fromJson(String source) =>
+      UserData.fromMap(json.decode(source) as Map<String, dynamic>);
+}
