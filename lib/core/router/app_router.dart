d5bdaa51640d68ed7d6c92977d4f6c9d87089bc0
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-05 17:51:29 +0200

    feat: implement logout logic in HomeView and verify session persistence

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media_app/core/router/app_routes.dart';
+import 'package:social_media_app/core/themes/app_colors.dart';
 import 'package:social_media_app/features/auth/views/auth_view.dart';
 import 'package:social_media_app/features/home/views/home_view.dart';
 
@@ -22,8 +23,21 @@ class AppRouter {
         return CupertinoPageRoute(
           builder:
               (_) => Scaffold(
-                body: Column(
-                  children: [Text('No route found ${settings.name}')],
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'No route found ${settings.name}',
+                        style: TextStyle(
+                          color: AppColors.black,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
           settings: settings,
--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -1,17 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
+import 'package:social_media_app/core/themes/background_theme_widget.dart';
+import 'package:social_media_app/core/widgets/custom_elevated_button.dart';
+import '../../../core/router/app_routes.dart';
+import '../../auth/logic/auth_cubit/auth_cubit.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text('Home View')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text('Home View')],
+    return BlocProvider(
+      create: (context) => AuthCubit(),
+      child: BlocListener<AuthCubit, AuthState>(
+        listener: (context, state) {
+          if (state is AuthSignedOut) {
+            Navigator.of(
+              context,
+              rootNavigator: true,
+            ).pushNamedAndRemoveUntil(AppRoutes.authRoute, (route) => false);
+          } else if (state is AuthFailure) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.errMsg)));
+          }
+        },
+        child: PopScope(
+          canPop: false,
+          child: Scaffold(
+            body: BackgroundThemeWidget(
+              child: Center(
+                child: Builder(
+                  builder: (context) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Gap(30),
+                        Text('Home View'),
+                        Gap(140),
+                        CustomElevatedButton(
+                          maximumSize: Size(150, 50),
+                          minimumSize: Size(150, 50),
+                          txtBtn: 'log out',
+                          onPressed: () {
+                            context.read<AuthCubit>().signOut();
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
