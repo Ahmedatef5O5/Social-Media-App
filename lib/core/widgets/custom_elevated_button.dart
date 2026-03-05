68cfbe6bcfed47d99a77d1b29c2128049b6b3a3a
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-05 13:26:42 +0200

    feat: implement CustomElevatedButton with loading state and custom borders

---
+++ lib/core/widgets/custom_elevated_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../themes/app_colors.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  const CustomElevatedButton({
+    super.key,
+    required this.txtBtn,
+    required this.onPressed,
+    this.bgColor,
+    this.txtColor,
+    this.isLoading = false,
+    this.minimumSize,
+    this.suffixIcon,
+    this.maximumSize,
+  });
+  final String txtBtn;
+  final Widget? suffixIcon;
+  final VoidCallback? onPressed;
+  final Color? bgColor;
+  final Color? txtColor;
+  final Size? minimumSize, maximumSize;
+  final bool isLoading;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: minimumSize ?? const Size(double.infinity, 55),
+        maximumSize: maximumSize ?? const Size(double.infinity, 55),
+        backgroundColor: bgColor ?? Theme.of(context).primaryColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        side: BorderSide(color: Theme.of(context).primaryColor, width: 1),
+      ),
+
+      onPressed: onPressed,
+      child:
+          suffixIcon ??
+          (isLoading
+              ? CupertinoActivityIndicator(color: AppColors.black12)
+              : Text(
+                txtBtn,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: txtColor ?? AppColors.white,
+                ),
+              )),
+    );
+  }
+}
