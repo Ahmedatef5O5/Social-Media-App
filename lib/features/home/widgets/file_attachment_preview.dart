388ab9af856c571253ce9e76514b95f798e07b99
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-09 15:24:48 +0200

    feat: add FileAttachmentPreview widget for displaying uploaded files in posts

---
+++ lib/features/home/widgets/file_attachment_preview.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import '../../../core/themes/app_colors.dart';
+
+class FileAttachmentPreview extends StatelessWidget {
+  final String url;
+  final void Function()? onTap;
+  const FileAttachmentPreview({super.key, required this.url, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    final fileName = url.split('/').last.split('?').first;
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: AppColors.grey3,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: AppColors.blueGrey1),
+        ),
+        child: Row(
+          children: [
+            const Icon(Icons.insert_drive_file, color: AppColors.primaryColor),
+            const Gap(10),
+            Expanded(
+              child: Text(
+                fileName,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            const Icon(Icons.download_rounded, size: 20, color: Colors.grey),
+          ],
+        ),
+      ),
+    );
+  }
+}
