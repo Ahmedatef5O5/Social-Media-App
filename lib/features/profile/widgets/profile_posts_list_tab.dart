649d36ad7623c0ca45a9ec7b89e0e40dd3d65d02
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-12 17:32:30 +0200

    refactor: implement separating ProfilePostsListTab with filtered posts from HomeCubit

---
+++ lib/features/profile/widgets/profile_posts_list_tab.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
+import 'package:social_media_app/core/themes/app_colors.dart';
+
+import '../../home/cubit/home_cubit.dart';
+import '../../home/widgets/post_item_widget.dart';
+
+class ProfilePostsListTab extends StatelessWidget {
+  final String userId;
+  const ProfilePostsListTab({super.key, required this.userId});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<HomeCubit, HomeState>(
+      builder: (context, state) {
+        if (state is PostsLoaded) {
+          final userPosts =
+              state.posts.where((p) => p.authorId == userId).toList();
+
+          if (userPosts.isEmpty) {
+            return const Center(child: Text('No posts yet'));
+          }
+
+          return ListView.separated(
+            padding: const EdgeInsets.all(16),
+            // physics: const NeverScrollableScrollPhysics(),
+            // shrinkWrap: true,
+            itemCount: userPosts.length,
+            itemBuilder:
+                (context, index) => PostItemWidget(post: userPosts[index]),
+            separatorBuilder: (BuildContext context, int index) => Gap(20),
+          );
+        }
+        return const Center(
+          child: CupertinoActivityIndicator(color: AppColors.black12),
+        );
+      },
+    );
+  }
+}
