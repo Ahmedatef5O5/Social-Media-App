85fbe2a98d65e70e9d0f7dc712c445f1e1caca57
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-12 15:18:29 +0200

    feat: integrate ProfileCubit with ProfileView to display real-time user data and stats

--- lib/features/profile/views/profile_view.dart
+++ lib/features/profile/views/profile_view.dart
@@ -1,5 +1,11 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
+import 'package:social_media_app/core/themes/app_colors.dart';
+import 'package:social_media_app/features/profile/cubit/profile_cubit.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../widgets/profile_header.dart';
+import '../widgets/proflie_states_widget.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -7,6 +13,39 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
-    return Center(child: Column(children: [ProfileHeader(size: size)]));
+    final userId = Supabase.instance.client.auth.currentUser!.id;
+    return BlocProvider(
+      create: (context) => ProfileCubit()..getProfileData(userId),
+      child: Center(
+        child: BlocBuilder<ProfileCubit, ProfileState>(
+          builder: (context, state) {
+            if (state is ProfileLoading) {
+              return Center(
+                child: CupertinoActivityIndicator(color: AppColors.black12),
+              );
+            } else if (state is ProfileLoaded) {
+              return MediaQuery.removePadding(
+                context: context,
+                removeTop: true,
+                child: SingleChildScrollView(
+                  padding: EdgeInsets.zero,
+                  child: Column(
+                    children: [
+                      ProfileHeader(size: size, user: state.user),
+                      Gap(20),
+                      ProfileStatsWidget(stats: state.stats),
+                    ],
+                  ),
+                ),
+              );
+            } else if (state is ProfileError) {
+              return Center(child: Text(state.message));
+            } else {
+              return SizedBox.shrink();
+            }
+          },
+        ),
+      ),
+    );
   }
 }
