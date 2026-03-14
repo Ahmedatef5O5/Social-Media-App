0f37196852b66a3ada6a604bd23fd0bf04c66e41
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-14 16:39:05 +0200

    feat(ui): implement image picking and upload logic in StoryItemWidget

--- lib/features/home/widgets/story_item_widget.dart
+++ lib/features/home/widgets/story_item_widget.dart
@@ -1,22 +1,93 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:social_media_app/features/home/cubit/home_cubit.dart';
 import 'package:social_media_app/features/home/models/story_model.dart';
 import '../../../core/themes/app_colors.dart';
 
 class StoryItemWidget extends StatelessWidget {
   final StoryModel? story;
   const StoryItemWidget({super.key, this.story});
+  void _showAddStoryOptions(BuildContext context) {
+    final homeCubit = context.read<HomeCubit>();
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder:
+          (context) => Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Add to Story',
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(),
+                ),
+                const Gap(20),
+                ListTile(
+                  leading: const Icon(
+                    Icons.photo_library,
+                    color: AppColors.primaryColor,
+                  ),
+                  title: const Text('Gallery'),
+                  onTap:
+                      () => _pickAndUpload(
+                        context,
+                        ImageSource.gallery,
+                        homeCubit,
+                      ),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.camera_alt, color: Colors.green),
+                  title: const Text('Take a Photo'),
+                  onTap:
+                      () => _pickAndUpload(
+                        context,
+                        ImageSource.camera,
+                        homeCubit,
+                      ),
+                ),
+              ],
+            ),
+          ),
+    );
+  }
+
+  Future<void> _pickAndUpload(
+    BuildContext context,
+    ImageSource source,
+    HomeCubit homeCubit,
+  ) async {
+    Navigator.pop(context);
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: source);
+    if (pickedFile != null) {
+      final file = File(pickedFile.path);
+      final userData = homeCubit.currentUserData;
+      if (userData != null) {
+        homeCubit.addStory(file: file, user: userData);
+      } else {
+        debugPrint('User data is null , cannot add story');
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
         if (story == null) {
+          _showAddStoryOptions(context);
           // Navigate to share story page
         } else {
           // Navigate to view story page
         }
       },
+
       child: Column(
         children: [
           Container(
