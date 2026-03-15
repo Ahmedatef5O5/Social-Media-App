fcd10e28ce756dc7895d3a9e2d31c8796cfbf8fc
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-15 05:59:41 +0200

    feat: implement native google sign-in and sync user data with db

--- lib/features/auth/data/repository/auth_repository.dart
+++ lib/features/auth/data/repository/auth_repository.dart
@@ -3,6 +3,8 @@ import 'package:social_media_app/features/auth/data/models/user_data.dart';
 abstract class AuthRepository {
   Future<void> signInWithEmail(String email, String password);
   Future<void> signUpWithEmail(String name, String email, String password);
+  Future<void> signInWithGoogle();
+  // Future<void> signInWithFacebook();
   Future<void> signOut();
   Future<void> resetPassword(String email);
   Future<UserData?> getUserData();
--- lib/features/auth/services/supabase_auth_services.dart
+++ lib/features/auth/services/supabase_auth_services.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:social_media_app/core/utilities/app_tables_names.dart';
 import 'package:social_media_app/features/auth/data/repository/auth_repository.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
 import '../data/models/user_data.dart';
 
 class SupabaseAuthServices implements AuthRepository {
@@ -39,9 +41,63 @@ class SupabaseAuthServices implements AuthRepository {
   }
 
   @override
+  Future<AuthResponse> signInWithGoogle() async {
+    try {
+      const webClientId =
+          '548020841452-cvtj4vs047g5acgtsmga02990tfagvg4.apps.googleusercontent.com';
+      final GoogleSignIn googleSignIn = GoogleSignIn(
+        serverClientId: webClientId,
+      );
+      final googleUser = await googleSignIn.signIn();
+
+      if (googleUser == null) {
+        throw 'Sign in aborted by user';
+        // return ;
+      }
+
+      final googleAuth = await googleUser.authentication;
+      final accessToken = googleAuth.accessToken;
+      final idToken = googleAuth.idToken;
+
+      if (idToken == null) {
+        throw 'No ID Token found';
+      }
+
+      final AuthResponse response = await _supabase.auth.signInWithIdToken(
+        provider: OAuthProvider.google,
+        idToken: idToken,
+        accessToken: accessToken,
+      );
+      final user = response.user;
+      if (user != null) {
+        final existingUser =
+            await _supabase
+                .from(AppTablesNames.users)
+                .select()
+                .eq(UserColumns.id, user.id)
+                .maybeSingle();
+        if (existingUser == null) {
+          await _setUserData(
+            user.userMetadata?[UserColumns.name] ?? 'Google User',
+            user.email!,
+            user.id,
+          );
+          debugPrint("New user added to 'users' table successfully.");
+        }
+      }
+      return response;
+    } catch (e) {
+      debugPrint('Google Sign-In Error: $e');
+      rethrow;
+    }
+  }
+
+  @override
   Future<void> signOut() async {
     try {
       await _supabase.auth.signOut();
+      final GoogleSignIn googleSignIn = GoogleSignIn();
+      await googleSignIn.signOut();
     } catch (e) {
       rethrow;
     }
