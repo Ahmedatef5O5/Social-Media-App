6b609a893c2fb21fc6c3a07693c3f6cadd36c8a7
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-16 17:45:55 +0200

    feat(drawer): implement navigation logic, ProfileCubit integration, and EditProfile redirection

--- lib/features/settings/widgets/profile_drawer.dart
+++ lib/features/settings/widgets/profile_drawer.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:social_media_app/core/constants/app_images.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 import 'package:social_media_app/core/themes/app_colors.dart';
+import 'package:social_media_app/features/auth/logic/auth_cubit/auth_cubit.dart';
+import 'package:social_media_app/features/profile/cubits/profile_cubit/profile_cubit.dart';
+import 'package:social_media_app/features/settings/widgets/drawer_item_widget.dart';
+import '../../../core/router/app_routes.dart';
+import 'drawer_header_widget.dart';
 
 class ProfileDrawer extends StatelessWidget {
-  const ProfileDrawer({super.key});
+  final PersistentTabController navController;
+  const ProfileDrawer({super.key, required this.navController});
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      backgroundColor: AppColors.white,
+      backgroundColor: AppColors.white.withValues(alpha: 0.92),
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(40),
@@ -18,54 +26,88 @@ class ProfileDrawer extends StatelessWidget {
       ),
       child: Column(
         children: [
-          _buildHeader(),
+          BlocBuilder<ProfileCubit, ProfileState>(
+            builder: (context, state) {
+              if (state is ProfileLoaded) {
+                return DrawerHeaderWidget(user: state.user);
+              } else {
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  child: CupertinoActivityIndicator(color: AppColors.black12),
+                );
+              }
+            },
+          ),
 
           const Divider(indent: 20, endIndent: 20),
           Expanded(
             child: ListView(
               padding: EdgeInsets.symmetric(horizontal: 10),
               children: [
-                _drawerItem(
+                DrawerItemWidget(
                   icon: CupertinoIcons.pen,
                   title: "Edit Profile",
-                  onTap: () {},
+                  onTap: () async {
+                    // Navigator.pop(context);
+                    final profileCubit = context.read<ProfileCubit>();
+                    final profileState = profileCubit.state;
+                    if (profileState is ProfileLoaded) {
+                      await Navigator.of(
+                        context,
+                        rootNavigator: true,
+                      ).pushNamed(
+                        AppRoutes.editProfileViewRoute,
+                        arguments: profileState.user,
+                      );
+                      if (context.mounted) {
+                        profileCubit.getProfileData(profileState.user.id);
+                      }
+                    }
+                  },
                 ),
-                _drawerItem(
+                DrawerItemWidget(
                   icon: Icons.people_outline,
-                  title: "Network",
-                  onTap: () {},
+                  title: "Discover People",
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    navController.jumpToTab(1);
+                  },
                 ),
-                _drawerItem(
+                DrawerItemWidget(
                   icon: Icons.photo_library_outlined,
                   title: "Photos/Videos",
                   onTap: () {},
                 ),
-                _drawerItem(
+                DrawerItemWidget(
                   icon: Icons.group_outlined,
                   title: "Group",
                   onTap: () {},
                 ),
-                _drawerItem(
+                DrawerItemWidget(
                   icon: Icons.lock_outline,
                   title: "Your Privacy",
                   onTap: () {},
                 ),
-                _drawerItem(
+                DrawerItemWidget(
                   icon: Icons.search,
                   title: "Search Profile",
                   onTap: () {},
                 ),
-                _drawerItem(
+                DrawerItemWidget(
                   icon: Icons.settings_outlined,
                   title: "Settings",
-                  onTap: () {},
+                  onTap:
+                      () => Navigator.of(
+                        context,
+                        rootNavigator: true,
+                      ).pushNamed(AppRoutes.settingsViewRoute),
                 ),
-                _drawerItem(
+                DrawerItemWidget(
                   icon: Icons.info_outline,
                   title: "About Us",
                   onTap: () {},
                 ),
-                _drawerItem(
+                DrawerItemWidget(
                   icon: Icons.language,
                   title: "Language",
                   onTap: () {},
@@ -73,64 +115,53 @@ class ProfileDrawer extends StatelessWidget {
               ],
             ),
           ),
-          _drawerItem(
-            icon: Icons.logout,
-            title: "Log Out",
-            color: Colors.red,
-            onTap: () {
-              // نادى على الـ AuthCubit هنا
+          BlocConsumer<AuthCubit, AuthState>(
+            listener: (context, state) {
+              if (state is AuthSignedOut) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('LogOut Successfully'),
+                    backgroundColor: Colors.redAccent,
+                    behavior: SnackBarBehavior.floating,
+                    duration: const Duration(seconds: 2),
+                  ),
+                );
+                Navigator.of(
+                  context,
+                  rootNavigator: true,
+                ).pushNamedAndRemoveUntil(
+                  AppRoutes.authRoute,
+                  (route) => false,
+                );
+              } else if (state is AuthFailure) {
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text(state.errMsg)));
+              }
+            },
+            buildWhen:
+                (previous, current) =>
+                    current is AuthSignedOut ||
+                    current is AuthFailure ||
+                    current is AuthLoading,
+            builder: (context, state) {
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: DrawerItemWidget(
+                  icon: Icons.logout,
+                  title: "Log Out",
+                  color: Colors.red,
+                  onTap:
+                      state is AuthLoading
+                          ? () {}
+                          : () => context.read<AuthCubit>().signOut(),
+                ),
+              );
             },
           ),
-          const SizedBox(height: 30),
+          const Gap(30),
         ],
       ),
-    );
-  }
-
-  Widget _buildHeader() {
-    return Container(
-      padding: const EdgeInsets.only(top: 60, bottom: 20),
-      child: Column(
-        children: [
-          const CircleAvatar(
-            radius: 45,
-            backgroundImage: NetworkImage(AppImages.defaultUserImg),
-          ),
-          const SizedBox(height: 15),
-          const Text(
-            "Dave C. Brown",
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-          const Text(
-            "@dave_brown",
-            style: TextStyle(color: Colors.grey, fontSize: 14),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _drawerItem({
-    required IconData icon,
-    required String title,
-    required VoidCallback onTap,
-    Color color = Colors.black87,
-  }) {
-    return ListTile(
-      leading: Icon(
-        icon,
-        color: color == Colors.red ? Colors.red : Colors.blue,
-      ),
-      title: Text(
-        title,
-        style: TextStyle(color: color, fontWeight: FontWeight.w500),
-      ),
-      trailing: const Icon(
-        Icons.arrow_forward_ios,
-        size: 16,
-        color: Colors.grey,
-      ),
-      onTap: onTap,
     );
   }
 }
