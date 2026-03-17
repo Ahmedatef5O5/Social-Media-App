7d7bb1ce29563280e9d6b48bd9a310d232fa6444
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-17 17:13:30 +0200

    feat: add text story option to StoryImagePickerSheet with StoryType enum

--- lib/features/home/widgets/story_image_picker_sheet.dart
+++ lib/features/home/widgets/story_image_picker_sheet.dart
@@ -3,10 +3,12 @@ import 'package:gap/gap.dart';
 import 'package:image_picker/image_picker.dart';
 import '../../../core/themes/app_colors.dart';
 
-class StoryImagePickerSheet extends StatelessWidget {
-  final Function(ImageSource source) onSourceSelected;
+enum StoryType { text, image }
 
-  const StoryImagePickerSheet({super.key, required this.onSourceSelected});
+class StoryImagePickerSheet extends StatelessWidget {
+  final Function(ImageSource? source, StoryType type) onSelected;
+
+  const StoryImagePickerSheet({super.key, required this.onSelected});
 
   @override
   Widget build(BuildContext context) {
@@ -22,17 +24,25 @@ class StoryImagePickerSheet extends StatelessWidget {
           const Gap(20),
           _buildBottomSheetOption(
             context,
+            icon: Icons.text_fields,
+            color: AppColors.primaryColor,
+            title: 'Text Story',
+            onTap: () => onSelected(null, StoryType.text),
+          ),
+
+          _buildBottomSheetOption(
+            context,
             icon: Icons.photo_library,
             color: AppColors.primaryColor,
             title: 'Gallery',
-            onTap: () => onSourceSelected(ImageSource.gallery),
+            onTap: () => onSelected(ImageSource.gallery, StoryType.image),
           ),
           _buildBottomSheetOption(
             context,
             icon: Icons.camera_alt,
             color: Colors.green,
             title: 'Take a Photo',
-            onTap: () => onSourceSelected(ImageSource.camera),
+            onTap: () => onSelected(ImageSource.camera, StoryType.image),
           ),
         ],
       ),
