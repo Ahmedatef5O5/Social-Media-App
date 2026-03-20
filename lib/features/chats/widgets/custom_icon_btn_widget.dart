ceacc46c0aec247da31fe15985d1337a192a2011
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-20 05:58:24 +0200

    feat: create CustomIconBtnWidget for reusable icon buttons used at (chat details view)

---
+++ lib/features/chats/widgets/custom_icon_btn_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../../../core/themes/app_colors.dart';
+
+class CustomIconBtnWidget extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onTap;
+  final double? size;
+  final double? radius;
+  final EdgeInsetsGeometry? padding;
+
+  const CustomIconBtnWidget({
+    super.key,
+    required this.icon,
+    required this.onTap,
+    this.size,
+    this.radius,
+    this.padding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: AppColors.transparent,
+      borderRadius: BorderRadius.circular(radius ?? 12),
+      onTap: onTap,
+      child: Padding(
+        padding: padding ?? EdgeInsets.zero,
+        child: Icon(
+          icon,
+          color: AppColors.primaryColor.withValues(alpha: 0.85),
+          size: size ?? 26,
+        ),
+      ),
+    );
+  }
+}
