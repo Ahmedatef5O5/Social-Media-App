85311cac15c6d5f7a6158b0b639d8566404a8dd8
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-21 21:43:13 +0200

    refactor: implement back to top functionality by converting HomeView to StatefulWidget and add scroll listener

--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:social_media_app/core/themes/background_theme_widget.dart';
+import 'package:social_media_app/core/widgets/custom_back_to_top_btn.dart';
 import 'package:social_media_app/core/widgets/custom_pull_to_refresh.dart';
 import 'package:social_media_app/features/home/cubit/home_cubit.dart';
 import '../widgets/home_view_header_section.dart';
@@ -9,39 +10,80 @@ import '../widgets/post_writing_card.dart';
 import '../widgets/posts_section.dart';
 import '../widgets/stories_list_section.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  late ScrollController _scrollController;
+  bool _showBackToTop = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (_scrollController.offset > 3000 && !_showBackToTop) {
+        setState(() => _showBackToTop = true);
+      } else if (_scrollController.offset <= 3000 && _showBackToTop) {
+        setState(() => _showBackToTop = false);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(
+      0,
+      duration: Duration(milliseconds: 600),
+      curve: Curves.easeOutCubic,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return PopScope(
       canPop: false,
       child: BackgroundThemeWidget(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: CustomPullToRefresh(
-              onRefresh:
-                  () async => await context.read<HomeCubit>().refreshHomeData(
-                    isRefresh: true,
+        child: Stack(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: CustomPullToRefresh(
+                  onRefresh:
+                      () async => await context
+                          .read<HomeCubit>()
+                          .refreshHomeData(isRefresh: true),
+                  child: CustomScrollView(
+                    controller: _scrollController,
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    slivers: [
+                      const SliverGap(30),
+                      SliverToBoxAdapter(child: HomeViewHeaderSection()),
+                      const SliverGap(35),
+                      SliverToBoxAdapter(child: PostWritingCard()),
+                      const SliverGap(20),
+                      SliverToBoxAdapter(child: StoriesListSection()),
+                      const SliverGap(4),
+                      PostsSection(),
+                      const SliverGap(25),
+                    ],
                   ),
-              child: CustomScrollView(
-                physics: const AlwaysScrollableScrollPhysics(),
-                scrollDirection: Axis.vertical,
-                slivers: [
-                  const SliverGap(30),
-                  SliverToBoxAdapter(child: HomeViewHeaderSection()),
-                  const SliverGap(35),
-                  SliverToBoxAdapter(child: PostWritingCard()),
-                  const SliverGap(20),
-                  SliverToBoxAdapter(child: StoriesListSection()),
-                  const SliverGap(4),
-                  PostsSection(),
-                  const SliverGap(25),
-                ],
+                ),
               ),
             ),
-          ),
+            CustomBackToTopBtn(isVisible: _showBackToTop, onTap: _scrollToTop),
+          ],
         ),
       ),
     );
