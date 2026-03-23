78e7d8bda501a98520180eac98370e202813f6dc
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-03-23 03:11:44 +0200

    feat: add StoryDisplayView with PageView for navigating between user stories

--- lib/features/home/views/story_display_view.dart
+++ lib/features/home/views/story_display_view.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/features/home/cubit/home_cubit.dart';
 import 'package:social_media_app/features/home/models/story_model.dart';
-import '../../../core/themes/app_colors.dart';
-import '../widgets/story_page_item.dart';
+import 'package:social_media_app/features/home/views/single_user_story_view.dart';
 
 class StoryDisplayView extends StatefulWidget {
   final List<StoryModel> stories;
   final int initialIndex;
+  final HomeCubit homeCubit;
 
   const StoryDisplayView({
     super.key,
     required this.stories,
     required this.initialIndex,
+    required this.homeCubit,
   });
 
   @override
@@ -35,29 +37,32 @@ class _StoryDisplayViewState extends State<StoryDisplayView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.black,
+      backgroundColor: Colors.black,
       body: PageView.builder(
         controller: _pageController,
         itemCount: widget.stories.length,
-        physics: const ClampingScrollPhysics(),
-        // physics: const AlwaysScrollableScrollPhysics(),
-        // physics: const PageScrollPhysics(),
-        onPageChanged: (value) {},
-        itemBuilder:
-            (context, index) => StoryPageItem(
-              story: widget.stories[index],
-              controller: _pageController,
-              onStoryComplete: () {
-                if (index < widget.stories.length - 1) {
-                  _pageController.nextPage(
-                    duration: const Duration(milliseconds: 400),
-                    curve: Curves.easeInOut,
-                  );
-                } else {
-                  Navigator.of(context).pop();
-                }
-              },
-            ),
+        physics: const BouncingScrollPhysics(),
+        // physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return SingleUserStoryView(
+            key: ValueKey(widget.stories[index].id),
+            story: widget.stories[index],
+            homeCubit: widget.homeCubit,
+            onComplete: () {
+              if (index < widget.stories.length - 1) {
+                _pageController.nextPage(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                );
+              } else {
+                Navigator.pop(context);
+              }
+            },
+            pageController: _pageController,
+            currentIndex: index,
+            totalCount: widget.stories.length,
+          );
+        },
       ),
     );
   }
