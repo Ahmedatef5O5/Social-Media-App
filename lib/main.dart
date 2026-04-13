9d2a5756eb9eab5c09b07888753aacd7a7931acb
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-04-13 12:00:00 +0200

    feat(app): integrate Firebase, notifications, and route tracking

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -8,10 +10,12 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:social_media_app/core/router/app_router.dart';
 import 'package:social_media_app/core/router/app_routes.dart';
 import 'package:social_media_app/core/secrets/app_secrets.dart';
+import 'package:social_media_app/core/services/active_screen_tracker.dart';
+import 'package:social_media_app/core/services/notification_services.dart';
 import 'package:social_media_app/core/themes/cubit/theme_cubit.dart';
 import 'package:social_media_app/features/auth/services/supabase_auth_services.dart';
+import 'package:social_media_app/firebase_options.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
 import 'features/auth/cubit/auth_cubit/auth_cubit.dart';
 
 void main() async {
@@ -27,10 +31,25 @@ void main() async {
   ]);
 
   await dotenv.load(fileName: '.env');
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    sound: true,
+  );
+
   await Supabase.initialize(
     url: AppSecrets.supabaseUrl,
     anonKey: AppSecrets.supabaseAnonKey,
   );
+
+  await NotificationService.instance.initialize();
 
   Supabase.instance.client.auth.onAuthStateChange.listen((data) {
     final session = data.session;
@@ -76,9 +95,28 @@ class MyApp extends StatelessWidget {
             initialRoute: AppRoutes.splashViewRoute,
             onGenerateRoute: AppRouter.generateRoute,
             onUnknownRoute: (settings) => AppRouter.generateRoute(settings),
+
+            navigatorKey: navigatorKey,
+
+            navigatorObservers: [_RouteObserver(), routeObserver],
           );
         },
       ),
     );
   }
+}
+
+class _RouteObserver extends NavigatorObserver {
+  void _update(Route? route) {
+    if (route?.settings.name != null) {
+      ActiveScreenTracker.setCurrentRoute(route!.settings.name!);
+    }
+  }
+
+  @override
+  void didPush(Route route, Route? previousRoute) => _update(route);
+  @override
+  void didPop(Route route, Route? previousRoute) => _update(previousRoute);
+  @override
+  void didReplace({Route? newRoute, Route? oldRoute}) => _update(newRoute);
 }
