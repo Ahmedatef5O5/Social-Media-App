71d7a6f172239a1de03eab6a6bed47c4101e61b9
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-04-17 14:26:34 +0200

    refactor(comments): extract layout constants for better maintainability

---
+++ lib/features/home/widgets/comment_constants.dart
@@ -0,0 +1,6 @@
+const double kIndent = 36.0;
+const double kReplySpacing = 10.0;
+const double kRepliesTopPad = 8.0;
+
+const double kAvatarDiameterBase = 36.0;
+const double kAvatarDiameterMin = 24.0;
