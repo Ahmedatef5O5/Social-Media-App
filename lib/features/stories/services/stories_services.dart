2501445a030c8b215d457cc8f472e680856bf010
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-04-18 19:12:36 +0200

    feat(stories): add separate upload method for story videos

--- lib/features/stories/services/stories_services.dart
+++ lib/features/stories/services/stories_services.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../../core/services/supabase_database_services.dart';
+import '../../../core/services/supabase_storage_services.dart';
 import '../../../core/utilities/supabase_constants.dart';
 import '../../home/models/story_model.dart';
 
 class StoriesServices {
   final _supabase = Supabase.instance.client;
   final supabaseServices = SupabaseDatabaseServices.instance;
+  final storageServices = SupabaseStorageServices();
 
   Future<String> uploadStoryFile(File file, String userId) async {
     final fileName =
@@ -16,19 +18,18 @@ class StoriesServices {
     return _supabase.storage.from(SupabaseConstants.stories).getPublicUrl(path);
   }
 
-  Future<void> createStory(StoryModel story) async {
-    await _supabase.from(SupabaseConstants.stories).insert(story.toMap());
-  }
+  Future<String> uploadStoryVideoFile(File file, String userId) async {
+    final fileName =
+        '${DateTime.now().millisecondsSinceEpoch}_${file.path.split('/').last}';
+    final path = '$userId/$fileName';
 
-  Future<void> deleteStory(String storyId) async {
-    try {
-      await _supabase
-          .from(SupabaseConstants.stories)
-          .delete()
-          .eq(StoryColumns.id, storyId);
-    } catch (e) {
-      rethrow;
-    }
+    await _supabase.storage
+        .from(SupabaseConstants.storyVideos)
+        .upload(path, file);
+
+    return _supabase.storage
+        .from(SupabaseConstants.storyVideos)
+        .getPublicUrl(path);
   }
 
   Future<List<StoryModel>> fetchStories() async {
@@ -44,6 +45,21 @@ class StoriesServices {
         builder: (data, id) => StoryModel.fromMap(data),
         primaryKey: StoryColumns.id,
       );
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> createStory(StoryModel story) async {
+    await _supabase.from(SupabaseConstants.stories).insert(story.toMap());
+  }
+
+  Future<void> deleteStory(String storyId) async {
+    try {
+      await _supabase
+          .from(SupabaseConstants.stories)
+          .delete()
+          .eq(StoryColumns.id, storyId);
     } catch (e) {
       rethrow;
     }
