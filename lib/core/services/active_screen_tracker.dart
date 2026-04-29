fe0f9b39e4d2108fa30e3a7ea5a7fff9ad970fb2
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-04-29 19:23:39 +0300

    feat(active-screen-group-call): add active group tracking support

--- lib/core/services/active_screen_tracker.dart
+++ lib/core/services/active_screen_tracker.dart
@@ -3,13 +3,21 @@ import 'package:social_media_app/core/router/app_routes.dart';
 class ActiveScreenTracker {
   static String? _currentRoute;
   static String? _activeChatReceiverId;
+  static String? _activeGroupId;
 
   static void setCurrentRoute(String route) => _currentRoute = route;
+
   static void setActiveChatReceiver(String? receiverId) =>
       _activeChatReceiverId = receiverId;
+
+  static void setActiveGroupId(String? groupId) => _activeGroupId = groupId;
 
   static bool isViewingChatWith(String senderId) {
     return _currentRoute == AppRoutes.chatDetailsViewRoute &&
         _activeChatReceiverId == senderId;
+  }
+
+  static bool isViewingGroup(String groupId) {
+    return _activeGroupId == groupId;
   }
 }
