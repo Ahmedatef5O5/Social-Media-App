5526059256641e074b520fb3a7a953ae4ccc1180
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-05-02 03:00:24 +0300

    feat(discover): add search action to discover people header

--- lib/features/discover/widgets/discover_people_header_section.dart
+++ lib/features/discover/widgets/discover_people_header_section.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:social_media_app/core/widgets/custom_header_widget.dart';
+import '../../../core/constants/app_images.dart';
+import '../views/search_view.dart';
 
 class DiscoverPeopleHeaderSection extends StatelessWidget {
   const DiscoverPeopleHeaderSection({super.key});
@@ -13,10 +15,35 @@ class DiscoverPeopleHeaderSection extends StatelessWidget {
         fontSize: 22,
         fontWeight: FontWeight.bold,
       ),
-      actions: Icon(
-        Icons.more_vert_outlined,
-        color: Theme.of(context).primaryColor,
-        size: 26,
+      actions: InkWell(
+        borderRadius: BorderRadius.circular(10),
+        onTap: () {
+          Navigator.of(context, rootNavigator: true).push(
+            PageRouteBuilder(
+              pageBuilder: (_, animation, __) => const SearchView(),
+              transitionsBuilder: (_, anim, __, child) {
+                return FadeTransition(
+                  opacity: anim,
+                  child: SlideTransition(
+                    position: Tween<Offset>(
+                      begin: const Offset(0, 0.05),
+                      end: Offset.zero,
+                    ).animate(
+                      CurvedAnimation(parent: anim, curve: Curves.easeOut),
+                    ),
+                    child: child,
+                  ),
+                );
+              },
+              transitionDuration: const Duration(milliseconds: 280),
+            ),
+          );
+        },
+        child: Image.asset(
+          AppImages.searchIcon,
+          width: 24,
+          color: Theme.of(context).primaryColor,
+        ),
       ),
     );
   }
