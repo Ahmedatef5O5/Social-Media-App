8efe5f0a31a04a73b1a6e47a02d6dd42c041d385
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-06-24 05:34:02 +0300

    feat: add FeedEvent model with various event types for post and presence changes

---
+++ lib/features/home/models/feed_event.dart
@@ -0,0 +1,33 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+sealed class FeedEvent {
+  const FeedEvent();
+}
+
+final class PostInsertedEvent extends FeedEvent {
+  final String postId;
+  const PostInsertedEvent(this.postId);
+}
+
+final class PostUpdatedEvent extends FeedEvent {
+  final String postId;
+  const PostUpdatedEvent(this.postId);
+}
+
+final class PostDeletedEvent extends FeedEvent {
+  final String postId;
+  const PostDeletedEvent(this.postId);
+}
+
+final class LikeChangedEvent extends FeedEvent {
+  final String postId;
+  final PostgresChangeEvent changeType;
+  const LikeChangedEvent(this.postId, this.changeType);
+}
+
+final class PresenceChangedEvent extends FeedEvent {
+  final String userId;
+  final bool isOnline;
+  final DateTime? updatedAt;
+  const PresenceChangedEvent(this.userId, this.isOnline, this.updatedAt);
+}
