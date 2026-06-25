6dbb3a5f162f1b7ccc86622f398b61877fc6ad3d
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-06-25 17:52:21 +0300

    feat: add AppAvatar widget for customizable user avatars with online status indicator

---
+++ lib/core/widgets/app_avatar.dart
@@ -0,0 +1,118 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:social_media_app/core/constants/app_images.dart';
+import 'package:social_media_app/core/widgets/custom_loading_indicator.dart';
+
+class AppAvatar extends StatelessWidget {
+  final String? imageUrl;
+  final double size;
+  final Color? borderColor;
+  final double borderWidth;
+  final bool showOnlineDot;
+  final bool isOnline;
+  final String? heroTag;
+  final VoidCallback? onTap;
+
+  const AppAvatar({
+    super.key,
+    this.imageUrl,
+    this.size = 36,
+    this.borderColor,
+    this.borderWidth = 2.0,
+    this.showOnlineDot = false,
+    this.isOnline = false,
+    this.heroTag,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Widget avatar = _buildAvatar(context);
+
+    if (heroTag != null) {
+      avatar = Hero(tag: heroTag!, child: avatar);
+    }
+
+    if (onTap != null) {
+      avatar = GestureDetector(onTap: onTap, child: avatar);
+    }
+
+    if (showOnlineDot) {
+      avatar = Stack(
+        clipBehavior: Clip.none,
+        children: [
+          avatar,
+          Positioned(bottom: 0, right: 0, child: _buildOnlineDot(context)),
+        ],
+      );
+    }
+
+    return avatar;
+  }
+
+  Widget _buildAvatar(BuildContext context) {
+    final hasBorder = borderColor != null;
+    final radius = size / 2;
+
+    Widget image = ClipOval(child: _buildImage(context));
+
+    if (hasBorder) {
+      return Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: borderColor!, width: borderWidth),
+        ),
+        child: image,
+      );
+    }
+
+    return SizedBox(width: size, height: size, child: image);
+  }
+
+  Widget _buildImage(BuildContext context) {
+    final hasUrl = imageUrl != null && imageUrl!.isNotEmpty;
+
+    if (!hasUrl) {
+      return Image.asset(
+        AppImages.defaultUserImg,
+        width: size,
+        height: size,
+        fit: BoxFit.cover,
+      );
+    }
+
+    return CachedNetworkImage(
+      imageUrl: imageUrl!,
+      width: size,
+      height: size,
+      fit: BoxFit.cover,
+      placeholder: (_, __) => CustomLoadingIndicator(radius: size / 4),
+      errorWidget:
+          (_, __, ___) => Image.asset(
+            AppImages.defaultUserImg,
+            width: size,
+            height: size,
+            fit: BoxFit.cover,
+          ),
+    );
+  }
+
+  Widget _buildOnlineDot(BuildContext context) {
+    final dotSize = (size * 0.28).clamp(8.0, 14.0);
+
+    return Container(
+      width: dotSize,
+      height: dotSize,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: isOnline ? Colors.green : Colors.grey,
+        border: Border.all(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          width: 1.5,
+        ),
+      ),
+    );
+  }
+}
