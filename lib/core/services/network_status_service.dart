8f3adc2860172ce78828401d91e48c919b3f1a1e
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-07-01 03:23:55 +0300

    feat: add NetworkStatusService for checking internet connectivity

---
+++ lib/core/services/network_status_service.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'dart:io';
+
+class NetworkStatusService {
+  NetworkStatusService._();
+
+  static final NetworkStatusService instance = NetworkStatusService._();
+
+  Future<bool> isConnected() async {
+    try {
+      final result = await InternetAddress.lookup(
+        'google.com',
+      ).timeout(const Duration(seconds: 5));
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    } on TimeoutException catch (_) {
+      return false;
+    } catch (_) {
+      return false;
+    }
+  }
+}
