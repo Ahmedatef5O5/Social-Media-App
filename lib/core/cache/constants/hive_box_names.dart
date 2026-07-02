7b98c5ede34885a985ee0b31771cb95221075e04
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-07-02 09:14:36 +0300

    feat: add HiveBoxNames and HiveTypeIds constants for media caching

---
+++ lib/core/cache/constants/hive_box_names.dart
@@ -0,0 +1,7 @@
+class HiveBoxNames {
+  const HiveBoxNames._();
+
+  static const String mediaCache = 'media_cache_box';
+
+  static const String cacheMeta = 'cache_meta_box';
+}
---
+++ lib/core/cache/constants/hive_type_ids.dart
@@ -0,0 +1,10 @@
+class HiveTypeIds {
+  const HiveTypeIds._();
+
+  static const int cachedMediaModel = 0;
+  static const int cachedMediaType = 1;
+
+  // Reserved for future cached entities (do not reuse these numbers):
+  // static const int cachedPostModel = 2;
+  // static const int cachedStoryModel = 3;
+}
