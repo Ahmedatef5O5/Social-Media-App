b36c53d9d0be15f70371e0e9f13a217b53d4d284
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-07-02 09:13:36 +0300

    feat: add CachedMediaModel and CachedMediaType for media caching

---
+++ lib/core/cache/models/cached_media_model.dart
@@ -0,0 +1,47 @@
+import 'package:hive/hive.dart';
+import '../constants/hive_type_ids.dart';
+part 'cached_media_model.g.dart';
+
+@HiveType(typeId: HiveTypeIds.cachedMediaType)
+enum CachedMediaType {
+  @HiveField(0)
+  image,
+  @HiveField(1)
+  video,
+  @HiveField(2)
+  raw,
+}
+
+@HiveType(typeId: HiveTypeIds.cachedMediaModel)
+class CachedMediaModel extends HiveObject {
+  @HiveField(0)
+  final String secureUrl;
+
+  @HiveField(1)
+  final CachedMediaType mediaType;
+
+  @HiveField(2)
+  final String featureFolder;
+
+  @HiveField(3)
+  final String? localFilePath;
+
+  @HiveField(4)
+  final DateTime cachedAt;
+
+  @HiveField(5)
+  DateTime lastAccessedAt;
+
+  @HiveField(6)
+  final int? sizeInBytes;
+
+  CachedMediaModel({
+    required this.secureUrl,
+    required this.mediaType,
+    required this.featureFolder,
+    required this.cachedAt,
+    required this.lastAccessedAt,
+    this.localFilePath,
+    this.sizeInBytes,
+  });
+}
---
+++ lib/core/cache/models/cached_media_model.g.dart
@@ -0,0 +1,103 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'cached_media_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CachedMediaModelAdapter extends TypeAdapter<CachedMediaModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  CachedMediaModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return CachedMediaModel(
+      secureUrl: fields[0] as String,
+      mediaType: fields[1] as CachedMediaType,
+      featureFolder: fields[2] as String,
+      cachedAt: fields[4] as DateTime,
+      lastAccessedAt: fields[5] as DateTime,
+      localFilePath: fields[3] as String?,
+      sizeInBytes: fields[6] as int?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, CachedMediaModel obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.secureUrl)
+      ..writeByte(1)
+      ..write(obj.mediaType)
+      ..writeByte(2)
+      ..write(obj.featureFolder)
+      ..writeByte(3)
+      ..write(obj.localFilePath)
+      ..writeByte(4)
+      ..write(obj.cachedAt)
+      ..writeByte(5)
+      ..write(obj.lastAccessedAt)
+      ..writeByte(6)
+      ..write(obj.sizeInBytes);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CachedMediaModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class CachedMediaTypeAdapter extends TypeAdapter<CachedMediaType> {
+  @override
+  final int typeId = 1;
+
+  @override
+  CachedMediaType read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return CachedMediaType.image;
+      case 1:
+        return CachedMediaType.video;
+      case 2:
+        return CachedMediaType.raw;
+      default:
+        return CachedMediaType.image;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, CachedMediaType obj) {
+    switch (obj) {
+      case CachedMediaType.image:
+        writer.writeByte(0);
+        break;
+      case CachedMediaType.video:
+        writer.writeByte(1);
+        break;
+      case CachedMediaType.raw:
+        writer.writeByte(2);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CachedMediaTypeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
