a73c97e50a93089af7a017285178d5fe7d1e92e8
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-07-22 23:02:12 +0300

    feat(reels): add HomeFeedWithReels widget for merging posts and reels in the feed

--- lib/features/home/views/home_view.dart
+++ lib/features/home/views/home_view.dart
@@ -8,12 +8,13 @@ import 'package:social_media_app/core/widgets/custom_back_to_top_btn.dart';
 import 'package:social_media_app/core/widgets/custom_pull_to_refresh.dart';
 import 'package:social_media_app/core/widgets/custom_tab_wrapper.dart';
 import 'package:social_media_app/features/home/cubits/home_cubit/home_cubit.dart';
+import 'package:social_media_app/features/reels/cubit/reels_feed_cubit/reels_feed_cubit.dart';
 import '../../../core/supabase/supabase_provider.dart';
 import '../../posts/cubit/posts_cubit/posts_cubit.dart';
+import '../../reels/widgets/home_feed_with_reels.dart';
 import '../../stories/cubit/stories_cubit/stories_cubit.dart';
 import '../widgets/home_view_header_section.dart';
 import '../../posts/widgets/post_writing_card.dart';
-import '../../posts/widgets/posts_section.dart';
 import '../../stories/widgets/stories_list_section.dart';
 import 'home_shimmer_skeleton_view.dart';
 
@@ -35,6 +36,10 @@ class _HomeViewState extends State<HomeView> {
   bool _isRefreshing = false;
   double _lastOffset = 0;
   bool _isScrollingToTop = false;
+  static const double _topScrollThreshold = 4.0;
+  bool get _isNearTop =>
+      !widget.scrollController.hasClients ||
+      widget.scrollController.offset <= _topScrollThreshold;
 
   @override
   void initState() {
@@ -113,6 +118,13 @@ class _HomeViewState extends State<HomeView> {
         context.read<PostsCubit>().refreshPosts(isRefresh: true),
         context.read<StoriesCubit>().fetchStories(isRefresh: true),
       ]);
+      if (!mounted) return;
+      final postsState = context.read<PostsCubit>().state;
+      if (postsState is PostsLoaded) {
+        await context.read<ReelsFeedCubit>().fetchReels(
+          postsCount: postsState.posts.length,
+        );
+      }
     } finally {
       if (mounted) {
         setState(() => _isRefreshing = false);
@@ -124,98 +136,142 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return PopScope(
       canPop: false,
-      child: BlocBuilder<HomeCubit, HomeState>(
-        builder: (context, homeState) {
-          return BlocBuilder<PostsCubit, PostsState>(
-            builder: (context, postsState) {
-              return BlocBuilder<StoriesCubit, StoriesState>(
-                builder: (context, storiesState) {
-                  return CustomTabWrapper(
-                    isLoading:
-                        homeState is HomeInitial ||
-                        homeState is UserDataLoading ||
-                        postsState is PostsInitial ||
-                        postsState is PostsLoading ||
-                        storiesState is StoriesLoading,
-                    errorMessage:
-                        homeState is UserDataLoadError
-                            ? homeState.message
-                            : (postsState is PostsLoadError
-                                ? postsState.message
-                                : null),
-
-                    onRetry: () {
-                      context.read<HomeCubit>().refreshUserData();
-                      context.read<PostsCubit>().refreshPosts();
-                    },
-                    loadingSkeleton: const HomeShimmerSkeleton(),
-
-                    child: Stack(
-                      children: [
-                        Center(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 12.0,
-                            ),
-                            child: CustomPullToRefresh(
-                              top: MediaQuery.sizeOf(context).height * 0.068,
-                              onRefresh: _handleRefresh,
-                              child: CustomScrollView(
-                                controller: widget.scrollController,
-                                physics: const AlwaysScrollableScrollPhysics(
-                                  parent: ClampingScrollPhysics(),
-                                ),
-                                scrollDirection: Axis.vertical,
-                                slivers: [
-                                  const SliverGap(44),
-                                  SliverToBoxAdapter(
-                                    child: HomeViewHeaderSection(
-                                      navController: widget.navController,
-                                    ),
-                                  ),
-                                  const SliverGap(35),
-                                  SliverToBoxAdapter(child: PostWritingCard()),
-                                  const SliverGap(20),
-                                  SliverToBoxAdapter(
-                                    child: StoriesListSection(),
-                                  ),
-                                  // const SliverGap(4),
-                                  PostsSection(),
-                                  SliverGap(
-                                    MediaQuery.of(context).padding.bottom + 100,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        if (_isRefreshing)
-                          Positioned.fill(
-                            child: IgnorePointer(
-                              child: Container(
-                                color:
-                                    Theme.of(context).scaffoldBackgroundColor,
-                                child: AnimatedOpacity(
-                                  duration: const Duration(milliseconds: 200),
-                                  opacity: 1,
-                                  child: const HomeShimmerSkeleton(),
-                                ),
-                              ),
-                            ),
-                          ),
-                        CustomBackToTopBtn(
-                          isVisible: _showBackToTop,
-                          onTap: _scrollToTop,
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              );
-            },
-          );
+      child: BlocListener<PostsCubit, PostsState>(
+        listenWhen: (previous, current) => current is PostsPendingUpdated,
+        listener: (context, state) {
+          if (_isNearTop) {
+            context.read<PostsCubit>().mergePendingPosts();
+          }
         },
+        child: BlocBuilder<HomeCubit, HomeState>(
+          builder: (context, homeState) {
+            return BlocBuilder<PostsCubit, PostsState>(
+              buildWhen: (previous, current) => current is! PostsPendingUpdated,
+
+              builder: (context, postsState) {
+                return BlocBuilder<StoriesCubit, StoriesState>(
+                  builder: (context, storiesState) {
+                    return CustomTabWrapper(
+                      isLoading:
+                          homeState is HomeInitial ||
+                          homeState is UserDataLoading ||
+                          postsState is PostsInitial ||
+                          postsState is PostsLoading ||
+                          storiesState is StoriesLoading,
+                      errorMessage:
+                          homeState is UserDataLoadError
+                              ? homeState.message
+                              : (postsState is PostsLoadError
+                                  ? postsState.message
+                                  : null),
+
+                      onRetry: () {
+                        context.read<HomeCubit>().refreshUserData();
+                        context.read<PostsCubit>().refreshPosts();
+                      },
+                      loadingSkeleton: const HomeShimmerSkeleton(),
+
+                      child: Stack(
+                        children: [
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 12.0,
+                              ),
+                              child: CustomPullToRefresh(
+                                top: MediaQuery.sizeOf(context).height * 0.068,
+                                onRefresh: _handleRefresh,
+                                child: CustomScrollView(
+                                  controller: widget.scrollController,
+                                  physics: const AlwaysScrollableScrollPhysics(
+                                    parent: ClampingScrollPhysics(),
+                                  ),
+                                  scrollDirection: Axis.vertical,
+                                  slivers: [
+                                    const SliverGap(44),
+                                    SliverToBoxAdapter(
+                                      child: HomeViewHeaderSection(
+                                        navController: widget.navController,
+                                      ),
+                                    ),
+                                    const SliverGap(35),
+                                    SliverToBoxAdapter(
+                                      child: PostWritingCard(),
+                                    ),
+                                    const SliverGap(20),
+                                    SliverToBoxAdapter(
+                                      child: StoriesListSection(),
+                                    ),
+                                    const HomeFeedWithReels(),
+                                    SliverGap(
+                                      MediaQuery.of(context).padding.bottom +
+                                          100,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          if (_isRefreshing)
+                            Positioned.fill(
+                              child: IgnorePointer(
+                                child: Container(
+                                  color:
+                                      Theme.of(context).scaffoldBackgroundColor,
+                                  child: AnimatedOpacity(
+                                    duration: const Duration(milliseconds: 200),
+                                    opacity: 1,
+                                    child: const HomeShimmerSkeleton(),
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                          Positioned(
+                            top: 22,
+                            left: 0,
+                            right: 0,
+                            child: Center(
+                              child: BlocBuilder<PostsCubit, PostsState>(
+                                buildWhen:
+                                    (previous, current) =>
+                                        current is PostsPendingUpdated ||
+                                        current is PostsLoaded,
+                                builder: (context, state) {
+                                  final pendingCount =
+                                      context
+                                          .read<PostsCubit>()
+                                          .pendingPosts
+                                          .length;
+                                  return _NewPostsPill(
+                                    count: pendingCount,
+                                    onTap: () {
+                                      context
+                                          .read<PostsCubit>()
+                                          .mergePendingPosts();
+                                      _scrollToTop();
+                                    },
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+
+                          CustomBackToTopBtn(
+                            isVisible: _showBackToTop,
+                            onTap: _scrollToTop,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
 }
+
---
+++ lib/features/reels/widgets/home_feed_with_reels.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
+import '../../../core/widgets/custom_loading_indicator.dart';
+import '../../posts/cubit/posts_cubit/posts_cubit.dart';
+import '../../posts/model/post_model.dart';
+import '../../posts/widgets/post_item_widget.dart';
+import '../cubit/reels_feed_cubit/reels_feed_cubit.dart';
+import '../model/reel_model.dart';
+import 'reels_horizontal_section.dart';
+
+class HomeFeedWithReels extends StatefulWidget {
+  const HomeFeedWithReels({super.key});
+
+  @override
+  State<HomeFeedWithReels> createState() => _HomeFeedWithReelsState();
+}
+
+class _HomeFeedWithReelsState extends State<HomeFeedWithReels> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _maybeFetchInitialReels(),
+    );
+  }
+
+  void _maybeFetchInitialReels() {
+    if (!mounted) return;
+    final postsState = context.read<PostsCubit>().state;
+    final reelsCubit = context.read<ReelsFeedCubit>();
+    if (postsState is PostsLoaded && reelsCubit.state is ReelsFeedInitial) {
+      reelsCubit.fetchReels(postsCount: postsState.posts.length);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final postsCubit = context.read<PostsCubit>();
+
+    return BlocListener<PostsCubit, PostsState>(
+      bloc: postsCubit,
+      listenWhen:
+          (previous, current) =>
+              current is PostsLoaded && previous is! PostsLoaded,
+      listener: (context, state) {
+        final reelsCubit = context.read<ReelsFeedCubit>();
+        if (reelsCubit.state is ReelsFeedInitial) {
+          reelsCubit.fetchReels(
+            postsCount: (state as PostsLoaded).posts.length,
+          );
+        }
+      },
+      child: BlocBuilder<PostsCubit, PostsState>(
+        bloc: postsCubit,
+        buildWhen:
+            (previous, current) =>
+                current is PostsLoading ||
+                current is PostsLoaded ||
+                current is PostsError,
+        builder: (context, postsState) {
+          return BlocBuilder<ReelsFeedCubit, ReelsFeedState>(
+            builder:
+                (context, reelsState) => _buildMergedSliver(
+                  context,
+                  postsCubit,
+                  postsState,
+                  reelsState,
+                ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildMergedSliver(
+    BuildContext context,
+    PostsCubit postsCubit,
+    PostsState postsState,
+    ReelsFeedState reelsState,
+  ) {
+    if (postsState is PostsLoading) {
+      return SliverToBoxAdapter(
+        child: SizedBox(
+          height: MediaQuery.sizeOf(context).height * 0.36,
+          child: const CustomLoadingIndicator(radius: 11),
+        ),
+      );
+    } else if (postsState is PostsLoaded) {
+      final posts = postsState.posts;
+      if (posts.isEmpty) {
+        return const SliverToBoxAdapter(
+          child: Center(child: Text('No posts available.')),
+        );
+      }
+
+      final injectionIndices =
+          reelsState is ReelsFeedLoaded
+              ? reelsState.injectionIndices
+              : const <int>[];
+      final sections =
+          reelsState is ReelsFeedLoaded
+              ? reelsState.sections
+              : const <List<ReelModel>>[];
+
+      final mergedItems = _mergeFeed(posts, injectionIndices, sections);
+
+      return SliverList.separated(
+        itemCount: mergedItems.length,
+        itemBuilder: (context, index) {
+          final item = mergedItems[index];
+          if (item.reelsSection != null) {
+            return ReelsHorizontalSection(reels: item.reelsSection!);
+          }
+          final post = item.post!;
+          return PostItemWidget(
+            key: ValueKey(post.id),
+            currPost: post,
+            postsCubit: postsCubit,
+          );
+        },
+        separatorBuilder: (context, index) => const Gap(14),
+      );
+    } else if (postsState is PostsError) {
+      return SliverToBoxAdapter(child: Center(child: Text(postsState.message)));
+    }
+    return const SliverToBoxAdapter(child: SizedBox.shrink());
+  }
+
+  List<_MergedFeedItem> _mergeFeed(
+    List<PostModel> posts,
+    List<int> injectionIndices,
+    List<List<ReelModel>> sections,
+  ) {
+    final items = <_MergedFeedItem>[];
+    var postCursor = 0;
+
+    for (var i = 0; i < injectionIndices.length; i++) {
+      final targetIndex = injectionIndices[i];
+      while (postCursor < targetIndex && postCursor < posts.length) {
+        items.add(_MergedFeedItem.post(posts[postCursor]));
+        postCursor++;
+      }
+      if (postCursor >= posts.length) break;
+      items.add(_MergedFeedItem.reels(sections[i]));
+    }
+
+    while (postCursor < posts.length) {
+      items.add(_MergedFeedItem.post(posts[postCursor]));
+      postCursor++;
+    }
+
+    return items;
+  }
+}
+
+class _MergedFeedItem {
+  final PostModel? post;
+  final List<ReelModel>? reelsSection;
+
+  const _MergedFeedItem.post(this.post) : reelsSection = null;
+  const _MergedFeedItem.reels(this.reelsSection) : post = null;
+}
