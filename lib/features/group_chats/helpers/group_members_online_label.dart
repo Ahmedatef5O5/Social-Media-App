e11eccc8116e3bf0ef602e35a3f1936ff35a5309
Ahmedatef5O5 <ahmedateif11@gmail.com>
2026-08-09 04:01:10 +0300

    feat(group-members): remove GroupSelectionCubit and add GroupMembersOnlineLabel with GroupHeaderStats model

---
+++ lib/features/group_chats/helpers/group_members_online_label.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../cubit/group_details_cubit/group_details_cubit.dart';
+import '../models/group_header_stats.dart';
+
+class GroupMembersOnlineLabel extends StatelessWidget {
+  final String groupId;
+  final TextStyle? style;
+  const GroupMembersOnlineLabel({super.key, required this.groupId, this.style});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<GroupHeaderStats>(
+      stream: context.read<GroupDetailsCubit>().watchHeaderStats(),
+      builder: (context, snapshot) {
+        final stats = snapshot.data;
+        final text =
+            stats == null
+                ? 'Tap for group info'
+                : '${stats.totalMembers} member${stats.totalMembers == 1 ? '' : 's'}'
+                    '${stats.onlineCount > 0 ? ', ${stats.onlineCount} online' : ''}';
+        return AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          transitionBuilder:
+              (child, anim) => FadeTransition(opacity: anim, child: child),
+          child: Text(
+            text,
+            key: ValueKey(text),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: style,
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/features/group_chats/models/group_header_stats.dart
@@ -0,0 +1,8 @@
+class GroupHeaderStats {
+  final int totalMembers;
+  final int onlineCount;
+  const GroupHeaderStats({
+    required this.totalMembers,
+    required this.onlineCount,
+  });
+}
